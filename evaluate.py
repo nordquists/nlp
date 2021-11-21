e0461af81df8fed6c16ec2beff075f36587e09c2
nordquists <srn334@nyu.edu>
2021-11-21 15:29:32 -0500

    first commit

---
+++ evaluate.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+
+import sys
+
+
+NUM_SAMPLES = 100
+
+def read_words_from_output_file():
+
+    pass
+
+def get_sample():
+    pass
+
+def write_evaluation_file():
+    pass
+
+
+
+if __name__ == '__main__':
+    output_filename = sys.argv[1]
+
+    words = read_words_from_output_file(output_filename)
+
+    sampled_words = get_sample(words)
+
+    write_evaluation_file(sampled_words)

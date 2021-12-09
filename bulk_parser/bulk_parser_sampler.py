25590ccb27df41846eb0b74461acd90c2d76938d
nordquists <srn334@nyu.edu>
2021-12-09 17:37:15 -0500

    sampler

--- bulk_parser/bulk_parser_sampler.py
+++ bulk_parser/bulk_parser_sampler.py
@@ -1,37 +1,66 @@
+import os
 import sys
 import glob
+from random import sample
+import shutil
 
+CPC_ORDERING = ['section',
+                'class',
+                'subclass',
+                'maingroup',
+                'subgroup']
 
+def decode_cpc(cpc): 
+    parameters = cpc.split('/')
+    cpc_dict = dict()
 
+    cpc_dict['section'] = parameters[0] if len(parameters) >= 1 else None
+    cpc_dict['class'] = parameters[1] if len(parameters) >= 2 else None
+    cpc_dict['subclass'] = parameters[2] if len(parameters) >= 3 else None
+    cpc_dict['maingroup'] = parameters[3] if len(parameters) >= 4 else None
+    cpc_dict['subgroup'] = parameters[4] if len(parameters) >= 5 else None
 
+    return cpc_dict
 
+def construct_cpc_path(cpc_dict):
+    path = ''
 
+    for parameter in CPC_ORDERING:
+        if cpc_dict[parameter]:
+            path += cpc_dict[parameter]
+            path += '/'
+        else:
+            return path
+    return path
 
-curr_path = ''
-# root_dir needs a trailing slash (i.e. /root/dir/)
-for filename in glob.iglob(root_dir + f'./{curr_path}/**/*.xml', recursive=True):
-     print(filename)
+def get_relevant_patents(directory, cpc_dict):
+    relevant_patents = []
+    cpc_path = construct_cpc_path(cpc_dict)
 
+    for filename in glob.iglob(f'{directory}/{cpc_path}**/*.xml', recursive=True):
+        relevant_patents.append(filename)
 
+    return relevant_patents
 
+def copy_relevant_patents(relevant_patents, output):
+    if not os.path.exists(output):
+        os.makedirs(output)
+    for f in relevant_patents:
+        shutil.copy(f, output + '/' + f.split('/')[-1])
 
+directory = sys.argv[1]
+cpc = sys.argv[2]
+num_to_sample = int(sys.argv[3])
+output = sys.argv[4]
 
+cpc_dict = decode_cpc(cpc)
 
-if __name__ == '__main__':
-    category_tree = sys.argv[1]
-    num_to_sample = sys.argv[2]
-    output_file = sys.argv[3]
+relevant_patents = get_relevant_patents(directory, cpc_dict)
 
-    categories_separated = category_tree.split('.')
+if len(relevant_patents) > num_to_sample:
+    sample_patents = sample(relevant_patents, num_to_sample)
+else:
+    print("WARNING: Number of patents too small for desired sample size. Continuing anyway.")
+    sample_patents = relevant_patents
 
-    sampled, num_sampled, total = sample_patents(categories_separated, int(num_to_sample))
-
-    if total == 0:
-        print("Error: category does not have any patents.")
-        exit()
-    elif total < int(num_to_sample):
-        print(f"Warning: not enough patents in that category to sample {num_to_sample} patents.")
-
-    write_sampled_patents(sampled)
-
-    print(f"Sampled {num_sampled} from a total of {total}.")
+copy_relevant_patents(sample_patents, output)

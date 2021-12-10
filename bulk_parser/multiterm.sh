b4f95bd42c6d64845ac71c6c1f9b2fbad1c346cb
nordquists <srn334@nyu.edu>
2021-12-09 21:43:33 -0500

    adjusted multiterm

--- bulk_parser/multiterm.sh
+++ bulk_parser/multiterm.sh
@@ -6,15 +6,11 @@
 ## $4 = LEVEL 2 FILE
 ## $5 = LEVEL 3 FILE
 ## $6 = LEVEL 4 FILE
-## $3 = BACKGROUND FILE PATH
-## $4 = OUTPUT NAME
-## $5 = BACKGROUND PICKLE FILE
 
 # We need to start 4 processes for each foreground corpus since
 # we are testing 4 different breadths for each. 
 
-bash ./nlp/bulk_parser/term.sh $1 $6 $5 "l4_l3"    "l3"    &
-bash ./nlp/bulk_parser/term.sh $1 $6 $4 "l4_l2"    "l2"    &
-bash ./nlp/bulk_parser/term.sh $1 $6 $3 "l4_l1"    "l1"    &
-bash ./nlp/bulk_parser/term.sh $1 $6 $2 "l4_base"  "base"  &
-wait
+bash ./nlp/bulk_parser/term.sh $1 $6 $5 "l4_l3"    "l3";
+bash ./nlp/bulk_parser/term.sh $1 $6 $4 "l4_l2"    "l2";
+bash ./nlp/bulk_parser/term.sh $1 $6 $3 "l4_l1"    "l1";
+bash ./nlp/bulk_parser/term.sh $1 $6 $2 "l4_base"  "base";

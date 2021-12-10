9ba65f0ce24ae8fdabd3487773886045cfcd89a7
nordquists <srn334@nyu.edu>
2021-12-09 21:28:07 -0500

    adjusted multiterm

--- bulk_parser/multiterm.sh
+++ bulk_parser/multiterm.sh
@@ -13,8 +13,8 @@
 # We need to start 4 processes for each foreground corpus since
 # we are testing 4 different breadths for each. 
 
-bash ./nlp/bulk_parser/term $1 $6 $5 "l4_l3"    "l3"    &
-bash ./nlp/bulk_parser/term $1 $6 $4 "l4_l2"    "l2"    &
-bash ./nlp/bulk_parser/term $1 $6 $3 "l4_l1"    "l1"    &
-bash ./nlp/bulk_parser/term $1 $6 $2 "l4_base"  "base"  &
+bash ./nlp/bulk_parser/term.sh $1 $6 $5 "l4_l3"    "l3"    &
+bash ./nlp/bulk_parser/term.sh $1 $6 $4 "l4_l2"    "l2"    &
+bash ./nlp/bulk_parser/term.sh $1 $6 $3 "l4_l1"    "l1"    &
+bash ./nlp/bulk_parser/term.sh $1 $6 $2 "l4_base"  "base"  &
 wait
